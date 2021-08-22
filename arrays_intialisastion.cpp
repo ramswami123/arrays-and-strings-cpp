19c527be7621d6cab3d8fb00ef3250c6ddff2080
ramswami123 <75007299+ramswami123@users.noreply.github.com>
2021-08-22 10:01:12 +0530

    Add files via upload

---
+++ arrays_intialisastion.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+    //intialising by size
+    int a[12];
+    //intialising by elements
+    int b[]={2,3,7,6};
+    //declaration by both size and elements
+    int c[4]={7,6,4,9};
+    return 0;
+}
